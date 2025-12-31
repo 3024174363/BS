d357e0152da8a0929125d79fdabac8d1cc4213ad
wangb <3024174363@qq.com>
2025-12-31 11:56:35 +0800

    sigma尝试以股价年度更新

--- BS.py
+++ BS.py
@@ -9,10 +9,11 @@ plt.rcParams['font.sans-serif'] = ['SimHei']
 plt.rcParams['axes.unicode_minus'] = False
  
 # 读取数据
-df_sita=pd.read_excel('合并_关键词得分汇总.xlsx')
-df_rate=pd.read_excel('三年期国债收益率.xlsx')
-df_total=pd.read_csv('EV2&MV&PB&TotalAsset.csv')
+df_sita=pd.read_excel('./data/input/合并_关键词得分汇总.xlsx')
+df_rate=pd.read_excel('./data/input/三年期国债收益率.xlsx')
+df_total=pd.read_csv('./data/input/EV2&MV&PB&TotalAsset.csv')
 
+df_sig=pd.read_csv('./data/input/比亚迪年sig.csv')
 #BS函数
 def BS(v_0,sigma,T,K,r):
     
@@ -21,28 +22,45 @@ def BS(v_0,sigma,T,K,r):
     d1 = (log(v_0/K) + (R + 0.5 * sigma**2) * T) / (sigma * sqrt(T))
     d2 = d1 - sigma * sqrt(T)
     call_price = v_0 * norm.cdf(d1) - K * exp(-R * T) * norm.cdf(d2)
-    #print('数据资产价格为{},d1为{},N(d1)为{},d2为{},N(d2)为{}'.format(call_price,d1,norm.cdf(d1),d2,norm.cdf(d2)))
+    print("-----------当前模型参数-----------")
+    print(f"当前V_0:{v_0}\n当前sigma:{sigma}\n当前T:{T}\n当前NDAT:{K}\n当前利率r:{r}")
+    print("-----------模型结果-----------")
+    print('数据资产价值为{},\nd1为{},N(d1)为{},\nd2为{},N(d2)为{}'.format(call_price,d1,norm.cdf(d1),d2,norm.cdf(d2)))
     return call_price,K * exp(-R * T),norm.cdf(d1),norm.cdf(d2)
 
 def caculate_sigma(symbol,yaer_num):
-    sigma = (df_total.loc[df_total["asharevalue_stat_symbol"] == symbol][-(yaer_num):]["balance_stat_total_assets"]).pct_change().mean()
+    #sigma = (df_total.loc[df_total["asharevalue_stat_symbol"] == symbol][-(yaer_num):]["asharevalue_ev2"]).pct_change().mean()
+    #sigma = (df_total.loc[df_total["asharevalue_stat_symbol"] == symbol][-(yaer_num):]["balance_stat_total_assets"]).pct_change().std()
+    #sigma1 = (df_total.loc[df_total["asharevalue_stat_symbol"] == symbol][-(yaer_num):]["balance_stat_total_assets"]).pct_change()[1:]
+    sigma = df_sig['sig'].values[-(yaer_num):]
+    #print("sigma:", sigma1)
     return sigma
 
 #返回K的时间序列
 def caculate_K(symbol,yaer_num):
-    return predict_arima(p=5,d=1,q=6,T=3,symbol="002594.SZ",yaer_num=11)
+    return predict_arima(p=5,d=1,q=6,T=3,symbol=symbol,yaer_num=11)
 
 
 def main_bs(symbol,yaer_num):
     v_0 = df_total.loc[df_total["asharevalue_stat_symbol"] == symbol][-(yaer_num):]["asharevalue_ev2"].values
     sigma = caculate_sigma(symbol,yaer_num)
-    K_series = caculate_K(symbol,yaer_num)
-    r = df_rate['rate_3'].values
+    K_series = caculate_K(symbol,yaer_num)[-(yaer_num+3):]
+    r = df_rate['rate_3'].values[-(yaer_num):]
     bs_results = []
     for i in range(yaer_num):
-        bs_result = BS(v_0[i],sigma,3,K_series[i+3],r[i])
+        print(f"--------------------------{2015+i}年--------------------------")
+        bs_result = BS(v_0[i],sigma[i],3,K_series[i+3],r[i])
         bs_results.append(bs_result[0])
-    print(v_0[-1])
     return bs_results
 
-print(main_bs(symbol="002594.SZ",yaer_num=11))
+a = main_bs(symbol="002594.SZ",yaer_num=10)
+#print(main_bs(symbol="600028.SH",yaer_num=10))
+
+def save_bs_results(a,symbol,yaer_num):
+    years = np.arange(2015,yaer_num+2015)
+
+    out = pd.DataFrame({'year': years, 'bs_price': a})
+    out.to_csv(f'./data/output/bs_prices_{symbol}_with_year.csv', index=False)
+    print(f'BS价格已保存到bs_prices_{symbol}_with_year.csv文件中。')
+
+save_bs_results(a,symbol="002594.SZ",yaer_num=10)
--- arima.py
+++ arima.py
@@ -9,7 +9,7 @@ plt.rcParams['font.sans-serif'] = ['Times New Roman']
 plt.rcParams['font.family'] = 'serif'
 plt.rcParams['axes.unicode_minus'] = False 
 
-def predict_arima(p=5,d=1,q=6,T=3,symbol="002594.SZ",yaer_num=11):
+def predict_arima(p,d,q,T,symbol,yaer_num):
     # 读取数据
     df_sita=pd.read_excel('合并_关键词得分汇总.xlsx')
     df_total=pd.read_csv('EV2&MV&PB&TotalAsset.csv')
@@ -17,10 +17,10 @@ def predict_arima(p=5,d=1,q=6,T=3,symbol="002594.SZ",yaer_num=11):
     df=df_total.loc[df_total["asharevalue_stat_symbol"] == symbol][-(yaer_num):]["asharevalue_ev2"]
 
     #23年再往前取10个
-    sl=df_sita[df_sita['stock_id']==2594]['关键词得分(%)_总和'][-10:].values
+    sl=df_sita[df_sita['stock_id']==int(symbol[0:6])]['关键词得分(%)_总和'][(1-yaer_num):].values
     extended_sl = 1-np.concatenate([
         sl,                                          # 原始数组
-        np.array([0.040869227149405])                # 末尾添加的1个值
+        np.array([0.040869227])                # 末尾添加的1个值
     ])
     s=extended_sl*df.values
 
@@ -31,6 +31,6 @@ def predict_arima(p=5,d=1,q=6,T=3,symbol="002594.SZ",yaer_num=11):
     forecast = results.forecast(steps=T)
 
     a = np.concatenate([s, forecast])
-
+    print("s", s)
     return a
-print(predict_arima(p=5,d=1,q=6,T=3))
+#print(predict_arima(p=5,d=1,q=6,T=3))
